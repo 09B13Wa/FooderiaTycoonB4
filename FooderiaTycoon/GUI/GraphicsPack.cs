fc698d2931e9569552303fd4fff037f23497abb0
jaschamerle <by8vbcs97b@privaterelay.appleid.com>
2021-06-20 17:30:02 +0200

    GraphicsPack.cs update

--- FooderiaTycoon/GUI/GraphicsPack.cs
+++ FooderiaTycoon/GUI/GraphicsPack.cs
@@ -7,10 +7,14 @@ namespace FooderiaTycoon.GUI
     public class GraphicsPack
     {
         private Dictionary<string, Bitmap> _relation;
+        private List<string> _model = new List<string>();
+        private FooderiaTycoon _game;
 
-        public GraphicsPack(Dictionary<string, Bitmap> relation)
+        public GraphicsPack(FooderiaTycoon game, List<string> model, Dictionary<string, Bitmap> relation)
         {
             _relation = relation;
+            _model = model;
+            _game = game;
         }
 
         public GraphicsPack(string path)
@@ -28,6 +32,11 @@ namespace FooderiaTycoon.GUI
         public static bool IsCompatible(string path)
         {
             return IsCompatible(new GraphicsPack(path));
+        }
+
+        private static bool BuildModel()
+        {
+            throw new NotImplementedException();
         }
     }
 }
