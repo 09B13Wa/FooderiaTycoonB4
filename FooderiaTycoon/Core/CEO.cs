c5c2a164cbbd7bf55edb833c4f16ee7dceb4282f
kiiro52o2 <57564151+kiiro52o2@users.noreply.github.com>
2021-06-24 16:13:42 +0200

    hey !

--- FooderiaTycoon/Core/CEO.cs
+++ FooderiaTycoon/Core/CEO.cs
@@ -1,9 +1,29 @@
-﻿namespace FooderiaTycoon.Core
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace FooderiaTycoon.Core
 {
     public struct CEO
     {
-        private string name;
+        private Pawn _pawn;
         private Company _company;
-        private int age;
+        
+        public CEO(Pawn pawn, Company company)
+        {
+            _pawn = pawn;
+            _company = company;
+        }
+
+        public Pawn Pawn
+        {
+            get => _pawn;
+            set => _pawn = value;
+        }
+
+        public Company Company
+        {
+            get => _company;
+            set => _company = value;
+        }
     }
+    
 }
--- FooderiaTycoon/Core/Company.cs
+++ FooderiaTycoon/Core/Company.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 
 namespace FooderiaTycoon.Core
 {
+    
     public class Company
     {
         protected string _name;
         protected int _inaugurationDate;
         protected CompanyColor _colorScheme;
-        protected string _type ;
         protected int _companyValue;
         protected int _brands;
         protected CEO _boss;
@@ -18,12 +18,40 @@ namespace FooderiaTycoon.Core
             _name = name;
             _inaugurationDate = inaugurationDate;
             _colorScheme = colorScheme;
-            _type = type ;
             _companyValue = companyValue;
             _brands = brands;
             _boss = boss;
         }
-        
+
+        public string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+
+        public CompanyColor ColorScheme
+        {
+            get => _colorScheme;
+            set => _colorScheme = value;
+        }
+
+        public int CompanyValue
+        {
+            get => _companyValue;
+            set => _companyValue = value;
+        }
+
+        public int Brands
+        {
+            get => _brands;
+            set => _brands = value;
+        }
+
+        public CEO Boss
+        {
+            get => _boss;
+            set => _boss = value;
+        }
         
     }
 }
