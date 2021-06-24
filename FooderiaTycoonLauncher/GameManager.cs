c865b5a5743a299464207e1d37eb3aceeb8d9d93
jaschamerle <by8vbcs97b@privaterelay.appleid.com>
2021-06-24 14:51:57 +0200

    update

---
+++ FooderiaTycoonLauncher/GameManager.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Launcher
+{
+    public class GameManager
+    {
+        private Install _launcherInstall;
+        private List<(Version version, Install install)> _gameInstalls;
+        private string _fooderiaDownloadUrl;
+
+        public GameManager(Install mainInstall, string fooderiaDownloadUrl)
+        {
+            _fooderiaDownloadUrl = fooderiaDownloadUrl;
+            _launcherInstall = mainInstall;
+            _gameInstalls = new List<(Version version, Install install)>();
+            ScanForInstalls();
+            CheckInstalls();
+        }
+
+        public string FooderiaDownloadUrl => _fooderiaDownloadUrl;
+
+        private void ScanForInstalls()
+        {
+            List<string> directories = Directory.EnumerateDirectories(_launcherInstall.AppendedRegularDirectory).ToList();
+            List<string> fooderiaTycoonDirectories = new List<string>();
+            foreach (string directory in directories)
+                if (IsFooderiaTycoonInstall(directory))
+                    fooderiaTycoonDirectories.Add(directory);
+            foreach (string fooderiaTycoonDirectory in fooderiaTycoonDirectories)
+                _gameInstalls.Add(ParseFooderiaTycoonInstall(fooderiaTycoonDirectory));
+        }
+
+        private void CheckInstalls()
+        {
+            foreach ((Version version, Install install) in _gameInstalls)
+                install.CheckInstall();
+        }
+
+        public bool IsVersionAvailable(Version version)
+        {
+            bool isAlreadyInstalled = _gameInstalls.Exists(tuple => tuple.version == version);
+            bool isAvailable;
+            try
+            {
+                string name = "FooderiaTycoon" + "_v_" + version.Major + "_" + version.Minor + "_" + version.Build;
+                Install install = new Install(name, FooderiaDownloadUrl + "_v_" + version.Major + "_" + version.Minor + "_" + version.Build);
+                isAvailable = true;
+            }
+            catch (ArgumentException e)
+            {
+                isAvailable = false;
+            }
+
+            return !isAlreadyInstalled && isAvailable;
+        }
+
+        public void AddVersion(Version version)
+        {
+            string name = "FooderiaTycoon" + "_v_" + version.Major + "_" + version.Minor + "_" + version.Build;
+            string downloadUrl = FooderiaDownloadUrl + "_v_" + version.Major + "_" + version.Minor + "_" + version.Build;
+            if (IsVersionAvailable(version))
+                _gameInstalls.Add((version, new Install(name, downloadUrl)));
+        }
+
+        public void RemoveVersion(Version version)
+        {
+            (Version version, Install install) tuple = _gameInstalls.Find(tuple => version == tuple.version);
+            _gameInstalls.Remove(tuple);
+            tuple.install.RemoveInstall();
+        }
+
+        private bool IsFooderiaTycoonInstall(string fooderiaTycoonSupposedInstall)
+        {
+            Match match = Regex.Match(fooderiaTycoonSupposedInstall, "FooderiaTycoon_v_\\d_\\d\\d_\\d\\d");
+            return match.Success;
+        }
+
+        public int CharToDigit(char c)
+        {
+            int digit;
+            switch (c)
+            {
+                case '0':
+                    digit = 0;
+                    break;
+                case '1':
+                    digit = 1;
+                    break;
+                case '2':
+                    digit = 2;
+                    break;
+                case '3':
+                    digit = 3;
+                    break;
+                case '4':
+                    digit = 4;
+                    break;
+                case '5':
+                    digit = 5;
+                    break;
+                case '6':
+                    digit = 6;
+                    break;
+                case '7':
+                    digit = 7;
+                    break;
+                case '8':
+                    digit = 8;
+                    break;
+                case '9':
+                    digit = 9;
+                    break;
+                default:
+                    throw new ArgumentException($"Error: expected a digit but got {c}");
+            }
+
+            return digit;
+        }
+
+        private (Version, Install) ParseFooderiaTycoonInstall(string fooderiaTycoonInstall)
+        {
+            int major = CharToDigit(fooderiaTycoonInstall[17]);
+            int minor = CharToDigit(fooderiaTycoonInstall[19]) * 10 + CharToDigit(fooderiaTycoonInstall[20]);
+            int build = CharToDigit(fooderiaTycoonInstall[22]) * 10 + CharToDigit(fooderiaTycoonInstall[23]);
+            Version version = new Version(major, minor, build);
+            Install install = new Install(fooderiaTycoonInstall,
+                FooderiaDownloadUrl + "_v_" + major + "_" + minor + "_" + build);
+            return (version, install);
+        }
+
+        private void LaunchVersion(Version version)
+        {
+            CheckInstalls();
+            if (_gameInstalls.Exists(tuple => tuple.version == version))
+            {
+               int location = _gameInstalls.FindIndex(tuple => tuple.version == version);
+               _gameInstalls[location].install.Launch(new string[0]);
+            }
+        }
+    }
+}
---
+++ FooderiaTycoonLauncher/Install.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO.Compression;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+
+namespace Launcher
+{
+    public class Install
+    {
+        private string _userName;
+        private string _firstDirectory;
+        private string _backupDirectory;
+        private Computer _computer;
+        private string _programName;
+        private string _downloadUrl;
+
+        public Install(string programName, string downloadUrl)
+        {
+            _programName = programName;
+            _userName = GetOsUserName();
+            _computer = new Computer();
+            _firstDirectory = _computer.DefaultDirectory;
+            _backupDirectory = _computer.BackupDirectory;
+            _downloadUrl = downloadUrl;
+            CheckDownloadUrl();
+        }
+        
+        public Install(string programName, string downloadUrl, string firstDirectory, string backupDirectory)
+        {
+            _programName = programName;
+            _userName = GetOsUserName();
+            _computer = new Computer();
+            _firstDirectory = firstDirectory;
+            _backupDirectory = backupDirectory;
+            _downloadUrl = downloadUrl;
+            CheckDownloadUrl();
+        }
+
+        public (byte[] hashBulk, string hashStr) GenerateFileHash(string path)
+        {
+            byte[] hash;
+            string hashStr;
+            using (MD5 md5 = MD5.Create())
+            {
+                using (FileStream stream = File.OpenRead(path))
+                {
+                    hash = md5.ComputeHash(stream);
+                    hashStr = BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
+                }
+            }
+
+            return (hash, hashStr);
+        }
+
+        public static string DefaultOsLocation => new Computer().GetDefaultDirectory();
+        public string OsPathSeperator => _computer.OsPathSeperator;
+
+        public string UserName => _userName;
+
+        public string RegularDirectory => _firstDirectory;
+        public string AppendedRegularDirectory => _firstDirectory + OsPathSeperator + _programName;
+        public string AppendedBackupDirectory => _firstDirectory + OsPathSeperator + _programName;
+
+        public Computer Computer => _computer;
+
+        public string ProgramName => _programName;
+        public string GetOsUserName()
+        {
+            return Environment.UserName;
+        }
+
+        private bool IsDefaultInstalled()
+        {
+            return Directory.Exists(AppendedRegularDirectory);
+        }
+
+        private static bool AreDirectoriesEqual(string firstDirectoryPath, string secondDirectoryPath)
+        {
+            DirectoryInfo dir1 = new DirectoryInfo(firstDirectoryPath);  
+            DirectoryInfo dir2 = new DirectoryInfo(secondDirectoryPath);  
+  
+            // Take a snapshot of the file system.  
+            IEnumerable<FileInfo> list1 = dir1.GetFiles("*.*", SearchOption.AllDirectories);  
+            IEnumerable<FileInfo> list2 = dir2.GetFiles("*.*", SearchOption.AllDirectories);  
+  
+            //A custom file comparer defined below  
+            FileCompare myFileCompare = new FileCompare();  
+  
+            // This query determines whether the two folders contain  
+            // identical file lists, based on the custom file comparer  
+            // that is defined in the FileCompare class.  
+            // The query executes immediately because it returns a bool.  
+            return list1.SequenceEqual(list2, myFileCompare);
+        }
+
+        private bool IsDefaultInstallIntact()
+        {
+            string newDirectory = AppendedRegularDirectory + "Temp";
+            InstallTo(newDirectory);
+            return AreDirectoriesEqual(AppendedRegularDirectory, newDirectory);
+        }
+        
+        private bool IsSecondaryInstallIntact()
+        {
+            string newDirectory = AppendedBackupDirectory + "Temp";
+            InstallTo(newDirectory);
+            return AreDirectoriesEqual(AppendedBackupDirectory, newDirectory);
+        }
+
+        private bool IsSecondaryInstalled()
+        {
+            return Directory.Exists(AppendedBackupDirectory);
+        }
+
+        public void CheckInstall()
+        {
+            if (!IsDefaultInstalled())
+                InstallToFirst();
+            else if (!IsDefaultInstallIntact())
+                ReplaceFirst();
+            if (!IsSecondaryInstalled())
+                InstallToSecond();
+            else if (!IsSecondaryInstallIntact())
+                ReplaceSecond();
+        }
+
+        private void InstallToFirst()
+        {
+            InstallTo(_firstDirectory);
+        }
+
+        private void InstallToSecond()
+        {
+            InstallTo(_backupDirectory);
+        }
+
+        private void InstallTo(string target)
+        {
+            using (var client = new WebClient())
+            {
+                client.DownloadFile(_downloadUrl, target + ".zip");
+            }
+
+            ZipFile.ExtractToDirectory(target + ".zip", target);
+            File.Delete(target + ".zip");
+        }
+
+        private void ReplaceFirst()
+        {
+            Directory.Delete(AppendedRegularDirectory);
+            InstallToFirst();
+        }
+
+        private void ReplaceSecond()
+        {
+            Directory.Delete(AppendedBackupDirectory);
+            InstallToFirst();
+        }
+
+        private void CheckDownloadUrl()
+        {
+            bool Test  = true;
+            Uri urlCheck = new Uri(_downloadUrl);
+            WebRequest request = WebRequest.Create(urlCheck);
+            request.Timeout = 15000;
+
+            WebResponse response;
+            try
+            {
+                response = request.GetResponse();
+            }
+            catch (Exception)
+            {
+                throw new ArgumentException("That is not a valid or known URL, sorry");
+            }
+        }
+
+        public void RemoveInstall()
+        {
+            Directory.Delete(AppendedRegularDirectory);
+            Directory.Delete(AppendedBackupDirectory);
+        }
+
+        public void Launch(string[] arguments)
+        {
+            Process.Start("notepad", "readme.txt");
+
+            string winpath = Environment.GetEnvironmentVariable("windir");
+            string path = AppendedRegularDirectory + OsPathSeperator + _programName + ".exe";
+
+            Process.Start(winpath + @"\Microsoft.NET\Framework\v1.0.3705\Installutil.exe", path);
+        }
+    }
+    
+    internal class FileCompare : System.Collections.Generic.IEqualityComparer<System.IO.FileInfo>  
+    {  
+        public FileCompare() { }  
+  
+        public bool Equals(System.IO.FileInfo f1, System.IO.FileInfo f2)  
+        {  
+            return (f1.Name == f2.Name &&  
+                    f1.Length == f2.Length);  
+        }  
+  
+        // Return a hash that reflects the comparison criteria. According to the
+        // rules for IEqualityComparer<T>, if Equals is true, then the hash codes must  
+        // also be equal. Because equality as defined here is a simple value equality, not  
+        // reference identity, it is possible that two or more objects will produce the same  
+        // hash code.  
+        public int GetHashCode(System.IO.FileInfo fi)  
+        {  
+            string s = $"{fi.Name}{fi.Length}";
+            return s.GetHashCode();  
+        }  
+    }  
+}
